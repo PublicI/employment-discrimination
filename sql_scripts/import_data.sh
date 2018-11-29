d2e5c9665024714f3e608423a5219069d528ca23
Joe Yerardi <joe.yerardi@gmail.com>
2018-11-28 20:14:46 -0500

    Delete files

--- sql_scripts/import_data.sh
+++
@@ -1,2 +0,0 @@
-psql -h db.fivetwentyseven.com -U postgres labor -f ./create_tables.sql
-psql -h db.fivetwentyseven.com -U postgres labor -f ./import_data.sql
--- sql_scripts/import_data.sql
+++
@@ -1,2 +0,0 @@
-\copy eeoc_complaints_11_17 FROM '../complaints_corrected_11_17.csv' WITH (FORMAT CSV, HEADER ON)
-\copy eeoc_complaints_92_11 FROM '../complaints_corrected_92_11.csv' WITH (FORMAT CSV, HEADER ON)
